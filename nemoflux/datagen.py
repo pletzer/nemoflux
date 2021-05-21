f9ba0f8f0be5eb69085e2fd59fff877269afa520
Alex Pletzer <alexander@gokliya.net>
2021-05-21 16:19:41 +1200

    running but need to fixe date line problem

--- nemoflux/datagen.py
+++ nemoflux/datagen.py
@@ -8,7 +8,7 @@ import geo
 # precision with which data will be saved in the netCDF file
 REAL = 'float64'
 
-class LatLonDataGen(object):
+class DataGen(object):
 
     def __init__(self, prefix=''):
         self.prefix = prefix
@@ -27,12 +27,17 @@ class LatLonDataGen(object):
         self.ny = ny
         self.nz = nz
 
-    def build(self, potentialFunction):
+    def build(self):
+        self.buildScaledVertical()
+        self.buildUniformHorizontal()
 
-        self.uExtensive = numpy.zeros((self.nz, self.ny, self.nx), REAL)
-        self.vExtensive = numpy.zeros((self.nz, self.ny, self.nx), REAL)
-        self.uArea = numpy.zeros((self.nz, self.ny, self.nx), REAL)
-        self.vArea = numpy.zeros((self.nz, self.ny, self.nx), REAL)
+    def buildScaledVertical(self):
+        #  non-uniform depth
+        self.zhalf = numpy.array([self.zmin + (self.zmax - self.zmin)*((k + 0.5)/float(self.nz))**2 for k in range(self.nz)])
+        self.ztop  = numpy.array([self.zmin + (self.zmax - self.zmin)*((k + 0  )/float(self.nz))**2 for k in range(self.nz)])
+        self.zbot  = numpy.array([self.zmin + (self.zmax - self.zmin)*((k + 1  )/float(self.nz))**2 for k in range(self.nz)])
+
+    def buildUniformHorizontal(self):
 
         ny1, nx1 = self.ny + 1, self.nx + 1
         dy, dx = (self.ymax - self.ymin)/float(self.ny), (self.xmax - self.xmin)/float(self.nx)
@@ -45,58 +50,58 @@ class LatLonDataGen(object):
         self.bounds_lat = numpy.zeros((self.ny, self.nx, 4), REAL)
         self.bounds_lon = numpy.zeros((self.ny, self.nx, 4), REAL)
 
-        #  non-uniform depth
-        self.zhalf = numpy.array([self.zmin + (self.zmax - self.zmin)*((k + 0.5)/float(self.nz))**2 for k in range(self.nz)])
-        self.ztop  = numpy.array([self.zmin + (self.zmax - self.zmin)*((k + 0  )/float(self.nz))**2 for k in range(self.nz)])
-        self.zbot  = numpy.array([self.zmin + (self.zmax - self.zmin)*((k + 1  )/float(self.nz))**2 for k in range(self.nz)])
+        # iterate over the horizontal cells
+        for j in range(self.ny):
+            y0 = self.ymin + j*dy
+            y1 = y0 + dy
+            ym = y0 + 0.5*dy
+            for i in range(self.nx):
+                x0 = self.xmin + i*dx
+                x1 = x0 + dx
+                xm = x0 + 0.5*dx
 
-        A = geo.EARTH_RADIUS # for evaluation
+                self.bounds_lat[j, i, 0] = y0
+                self.bounds_lat[j, i, 1] = y0
+                self.bounds_lat[j, i, 2] = y1
+                self.bounds_lat[j, i, 3] = y1
 
-        # iterate over cells
+                self.bounds_lon[j, i, 0] = x0
+                self.bounds_lon[j, i, 1] = x1
+                self.bounds_lon[j, i, 2] = x1
+                self.bounds_lon[j, i, 3] = x0
+
+                # cell mid point
+                self.nav_lat[j, i] = ym
+                self.nav_lon[j, i] = xm
+
+    def applyPotential(self, potentialFunction):
+        self.potential = numpy.zeros((self.nz, self.ny, self.nx, 4), numpy.float64)
+        A = geo.EARTH_RADIUS
         for k in range(self.nz):
-            dz = self.ztop[k] - self.zbot[k]
-            z = self.zhalf[k] # z is at half level
+            z = self.zhalf[k]
             for j in range(self.ny):
-                y0 = self.ymin + j*dy
-                y1 = y0 + dy
-                ym = y0 + 0.5*dy
                 for i in range(self.nx):
-                    x0 = self.xmin + i*dx
-                    x1 = x0 + dx
-                    xm = x0 + 0.5*dx
+                    for vertex in range(4):
+                        y = self.bounds_lat[j, i, vertex]
+                        x = self.bounds_lon[j, i, vertex]
+                        self.potential[k, j, i, vertex] = eval(potentialFunction)
 
-                    self.bounds_lat[j, i, 0] = y0
-                    self.bounds_lat[j, i, 1] = y0
-                    self.bounds_lat[j, i, 2] = y1
-                    self.bounds_lat[j, i, 3] = y1
-
-                    self.bounds_lon[j, i, 0] = x0
-                    self.bounds_lon[j, i, 1] = x1
-                    self.bounds_lon[j, i, 2] = x1
-                    self.bounds_lon[j, i, 3] = x0
-
-                    self.nav_lat[j, i] = ym
-                    self.nav_lon[j, i] = xm
-
-                    # east side of the cell
-                    x, y = x1, y1,
-                    p1 = numpy.array((x, y, 0.))
-                    phi1 = eval(potentialFunction)
-                    x, y = x1, y0
-                    p0 = numpy.array((x, y, 0.))
-                    phi0 = eval(potentialFunction)
-                    self.uExtensive[k, j, i] = (phi1 - phi0) * dz
-                    self.uArea[k, j, i] = geo.getArcLength(p0, p1,  radius=geo.EARTH_RADIUS) * dz
-
-                    # north side of the cell
-                    x, y = x1, y1
-                    p1 = numpy.array((x, y, 0.))
-                    phi1 = eval(potentialFunction)
-                    x, y = x0, y1
-                    p0 = numpy.array((x, y, 0.))
-                    phi0 = eval(potentialFunction)
-                    self.vExtensive[k, j, i] = (phi1 - phi0) * dz
-                    self.vArea[k, j, i] = geo.getArcLength(p0, p1, radius=geo.EARTH_RADIUS) * dz
+    def computeUVFromPotential(self):
+        self.u = numpy.zeros((self.nz, self.ny, self.nx), numpy.float64)
+        self.v = numpy.zeros((self.nz, self.ny, self.nx), numpy.float64)
+        for k in range(self.nz):
+            for j in range(self.ny):
+                for i in range(self.nx):
+                    x0, x1, x2, x3 = self.bounds_lon[j, i, :]
+                    y0, y1, y2, y3 = self.bounds_lat[j, i, :]
+                    p0 = numpy.array((x0, y0, 0.))
+                    p1 = numpy.array((x1, y1, 0.))
+                    p2 = numpy.array((x2, y2, 0.))
+                    p3 = numpy.array((x3, y3, 0.))
+                    # east
+                    self.u[k, j, i] = -(self.potential[k, j, i, 2] - self.potential[k, j, i, 1]) / geo.getArcLength(p2, p1, radius=geo.EARTH_RADIUS)
+                    # north
+                    self.v[k, j, i] = -(self.potential[k, j, i, 2] - self.potential[k, j, i, 3]) / geo.getArcLength(p2, p3, radius=geo.EARTH_RADIUS)
 
     def rotatePole(self, deltaLonDeg=0., deltaLatDeg=0.):
 
@@ -126,7 +131,6 @@ class LatLonDataGen(object):
 
         for j in range(self.ny):
             for i in range(self.nx):
-
                 the = numpy.pi * self.nav_lat[j, i] / 180.
                 lam = numpy.pi * self.nav_lon[j, i] / 180.
                 cos_the = numpy.cos(the)
@@ -140,11 +144,10 @@ class LatLonDataGen(object):
 
                 self.nav_lat[j, i] = 180. * math.asin(xyz1[2]) / pi
                 self.nav_lon[j, i] = 180. * math.atan2(xyz1[1], xyz1[0]) / pi
-                if self.nav_lon[j, i] < 0.:
-                    self.nav_lon[j, i] += 360.
+                # use the convention 0 <= lon < 360
+                self.nav_lon[j, i] %= 360.
 
                 for vertex in range(4):
-
                     the = numpy.pi * self.bounds_lat[j, i, vertex] / 180.
                     lam = numpy.pi * self.bounds_lon[j, i, vertex] / 180.
                     cos_the = numpy.cos(the)
@@ -158,35 +161,25 @@ class LatLonDataGen(object):
 
                     self.bounds_lat[j, i, vertex] = 180. * math.asin(xyz1[2]) / numpy.pi
                     self.bounds_lon[j, i, vertex] = 180. * math.atan2(xyz1[1], xyz1[0]) / numpy.pi
-                    if self.bounds_lon[j, i, vertex] < 0:
-                        self.bounds_lon[j, i, vertex] += 360.
-
-                    # add/subtract 360 deg to make the cell well behaved
-                    if vertex > 0 and self.bounds_lon[j, i, vertex] - self.bounds_lon[j, i, 0] < -270.:
-                        self.bounds_lon[j, i, vertex] += 360.
-                    if vertex > 0 and self.bounds_lon[j, i, vertex] - self.bounds_lon[j, i, 0] > +270.:
-                        self.bounds_lon[j, i, vertex] -= 360.
-
-        # compute the face areas
-        for k in range(self.nz):
-            dz = self.ztop[k] - self.zbot[k]
-            for j in range(self.ny):
-                for i in range(self.nx):
-                    # east side of the cell
-                    p1 = numpy.array((self.bounds_lon[j, i, 2], self.bounds_lon[j, i, 2], 0.))
-                    p0 = numpy.array((self.bounds_lon[j, i, 1], self.bounds_lon[j, i, 1], 0.))
-                    self.uArea[k, j, i] = geo.getArcLength(p0, p1, radius=geo.EARTH_RADIUS) * dz
-                    # north side of the cell
-                    p1 = numpy.array((self.bounds_lon[j, i, 3], self.bounds_lon[j, i, 3], 0.))
-                    p0 = numpy.array((self.bounds_lon[j, i, 2], self.bounds_lon[j, i, 2], 0.))
-                    self.vArea[k, j, i] = geo.getArcLength(p0, p1, radius=geo.EARTH_RADIUS) * dz
+                    # use the convention 0 <= lon < 360
+                    self.bounds_lon[j, i, vertex] %= 360.
 
     def save(self):
 
         ncT = netCDF4.Dataset(self.prefix + '_T.nc', 'w')
+        ncT.createDimension('z', self.nz)
         ncT.createDimension('y', self.ny)
         ncT.createDimension('x', self.nx)
         ncT.createDimension('nvertex', 4)
+        ncT.createDimension('axis_nbounds', 2)
+        deptht = ncT.createVariable('deptht', REAL, ('z',))
+        deptht.standard_name = 'depth'
+        deptht.units = 'm'
+        deptht.bounds = 'depthu_bounds'
+        deptht[:] = self.zhalf
+        deptht_bounds = ncT.createVariable('deptht_bounds', REAL, ('z', 'axis_nbounds'))
+        deptht_bounds[:, 0] = self.ztop
+        deptht_bounds[:, 1] = self.zbot
 
         nav_lat = ncT.createVariable('nav_lat', REAL, ('y', 'x'))
         nav_lat.standard_name = 'latitude'
@@ -203,6 +196,10 @@ class LatLonDataGen(object):
 
         bounds_lon = ncT.createVariable('bounds_lon', REAL, ('y', 'x', 'nvertex'))
         bounds_lon[:] = self.bounds_lon
+
+        potential = ncT.createVariable('potential', REAL, ('z', 'y', 'x', 'nvertex'))
+        potential.long_name = 'potential'
+        potential[:] = self.potential
         ncT.close()
 
         ncU = netCDF4.Dataset(self.prefix + '_U.nc', 'w')
@@ -218,14 +215,10 @@ class LatLonDataGen(object):
         depthu_bounds = ncU.createVariable('depthu_bounds', REAL, ('z', 'axis_nbounds'))
         depthu_bounds[:, 0] = self.ztop
         depthu_bounds[:, 1] = self.zbot
-        uArea = ncU.createVariable('uArea', REAL, ('z', 'y', 'x'))
-        uArea.long_name = 'u face area'
-        uArea.units = 'm2'
-        uArea[:] = self.uArea
         uo = ncU.createVariable('uo', REAL, ('z', 'y', 'x'), fill_value=1.e20)
         uo.standard_name = 'sea_water_x_velocity'
         uo.units = 'm/s'
-        uo[:] = self.uExtensive / self.uArea
+        uo[:] = self.u
         ncU.earthRadius = f'earth radius = {geo.EARTH_RADIUS} in metres'
         ncU.close()
 
@@ -239,17 +232,13 @@ class LatLonDataGen(object):
         depthv.units = 'm'
         depthv.bounds = 'depthv_bounds'
         depthv[:] = self.zhalf
-        vArea = ncV.createVariable('vArea', REAL, ('z', 'y', 'x'))
-        vArea.long_name = 'v face area'
-        vArea.units = 'm2'
-        vArea[:] = self.vArea
         depthv_bounds = ncV.createVariable('depthv_bounds', REAL, ('z', 'axis_nbounds'))
         depthv_bounds[:, 0] = self.ztop
         depthv_bounds[:, 1] = self.zbot
         vo = ncV.createVariable('vo', REAL, ('z', 'y', 'x'), fill_value=1.e20)
         vo.standard_name = 'sea_water_y_velocity'
         vo.units = 'm/s'
-        vo[:] = self.vExtensive / self.vArea
+        vo[:] = self.v
         ncV.close()
 
 
@@ -266,11 +255,13 @@ def main(*, potentialFunction: str="(1. - z/self.zmax)*x*2*pi*A/360.", prefix: s
     :param ny: number of cells in latitude
     :param potentialFunction: potential expression of x and y
     """
-    lldg = LatLonDataGen(prefix)
+    lldg = DataGen(prefix)
     lldg.setSizes(nx, ny, nz)
     lldg.setBoundingBox(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, zmin=0., zmax=1000.)
-    lldg.build(potentialFunction)
+    lldg.build()
     lldg.rotatePole(deltaLonDeg=deltaLonDeg, deltaLatDeg=deltaLatDeg)
+    lldg.applyPotential(potentialFunction)
+    lldg.computeUVFromPotential()
     lldg.save()
 
 if __name__ == '__main__':
--- nemoflux/horizgrid.py
+++ nemoflux/horizgrid.py
@@ -13,52 +13,36 @@ class HorizGrid(object):
         # read the cell bounds
         bounds_lat = nc.variables['bounds_lat'][:]
         bounds_lon = nc.variables['bounds_lon'][:]
+        # read the potential values
+        potential = nc.variables['potential'][:]
         nc.close()
 
         ny, nx, nvertex = bounds_lat.shape
         numCells = ny * nx
         self.points = numpy.zeros((numCells, 4, 3), numpy.float64)
+        self.cellAreas = numpy.zeros((numCells,), numpy.float64)
 
-        k = 0
+        cellId = 0
         for j in range(ny):
             for i in range(nx):
-                for vertex in range(4):
-                    x, y = bounds_lon[j, i, vertex], bounds_lat[j, i, vertex]
-                    self.points[k, vertex, :] = x, y, 0.0
-                k += 1
+                for vertex0 in range(4):
+                    vertex1 = (vertex0 + 1) % 4
+                    vertex2 = (vertex0 - 1) % 4
+                    x0, y0 = bounds_lon[j, i, vertex0], bounds_lat[j, i, vertex0]
+                    x1, y1 = bounds_lon[j, i, vertex1], bounds_lat[j, i, vertex1]
+                    x2, y2 = bounds_lon[j, i, vertex2], bounds_lat[j, i, vertex2]
+                    self.points[cellId, vertex0, :] = x0, y0, 0.0
+                    dp10 = numpy.array((x1 - x0, y1 - y0, 0.))
+                    dp20 = numpy.array((x2 - x0, y2 - y0, 0.))
+                    area = numpy.cross(dp10, dp20)
+                    self.cellAreas[cellId] += 0.25*area[2]
+                # increment the cell counter
+                cellId += 1
 
         self.grid = mint.Grid()
         self.grid.setPoints(self.points)
         self.grid.computeEdgeArcLengths()
-
-        # compute the areas
-        dx10 = self.points[:, 1, 0] - self.points[:, 0, 0]
-        dy10 = self.points[:, 1, 1] - self.points[:, 0, 1]
-        dx30 = self.points[:, 3, 0] - self.points[:, 0, 0]
-        dy30 = self.points[:, 3, 1] - self.points[:, 0, 1]
-        self.areas0 = dx10*dy30 - dx30*dy10
-
-        dx32 = self.points[:, 3, 0] - self.points[:, 2, 0]
-        dy32 = self.points[:, 3, 1] - self.points[:, 2, 1]
-        dx12 = self.points[:, 1, 0] - self.points[:, 2, 0]
-        dy12 = self.points[:, 1, 1] - self.points[:, 2, 1]
-        self.areas2 = dx32*dy12 - dx12*dy32
-
-        self.grid.attach('areas0', self.areas0)
-        self.grid.attach('areas2', self.areas2)
-        
-    def getEdgeArcLength(self, cellId, edgeIndex):
-    	return self.grid.getEdgeArcLength(cellId, edgeIndex)
-
-    def getMintGrid(self):
-        return self.grid
-
-    def getNumCells(self):
-        return self.points.shape[0]
-
-    def getPoint(self, k, vertex):
-        return self.points[k, vertex, :]
-
+        self.grid.attach('cellAreas', self.cellAreas)
 
     def dump(self, fileName):
         """Dump the grid data to a VTK file
