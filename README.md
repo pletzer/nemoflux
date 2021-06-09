8ab29b0d7ea525ebbd046de0984525beefbb0a1d
Alex Pletzer <alexander@gokliya.net>
2021-06-09 16:36:54 +1200

    more instructions

--- README.md
+++ README.md
@@ -10,12 +10,13 @@ conda create -n nemoflux python=3.8
 conda activate nemoflux
 conda install -c conda-forge python-mint>=1.14.6
 ```
+**NOTE: python-mint version 1.14.6 should become available soon**
 
 ## How to download the software
 
 ```
 git clone https://github.com/pletzer/nemoflux
-
+cd nemoflux/nemoflux
 ```
 
 ## A quick tour of nemoflux
@@ -28,25 +29,29 @@ Will produce three files:
 ```
 ls test_?.nc
 ```
-should display test_T.nc, test_U.nc	and test_V.nc. test_T.nc contains the grid information, test_U.nc the u velocity component and test_V.nc the v velocity component. The following command will display the total flow for the target longitude, latitude points (-129,-80),(-23,-34),(156,78):
+should display test_T.nc, test_U.nc	and test_V.nc. test_T.nc contains the grid information, test_U.nc and test_V.nc contain the u, v velocity components. The following command will display the total flow for the target longitude, latitude points (-129,-80),(-23,-34),(156,78):
 ```
 python fluxviz.py  -t test_T.nc -u test_U.nc -v test_V.nc -l "(-129,-80),(-23,-34),(156,78)"
 ```
 
 ![alt total flow at time 0](https://github.com/pletzer/nemoflux/blob/main/picture/simple.png?raw=true)
 
-You can step in time by typing "t" in the window. Type "q" to quit/exit.
+You can step in time by typing "t" in the window. Type "q" to quit/exit. Zoom in/out using the mouse/pad. To reset the view type "r".
+
 
 ## How to subset NEMO data
 
-TO DO
-
+To subset the NEMO data to a smaller domain
+```
+python python subsetNEMO.py -t $TFILE -u $UFILE -v $VFILE --outputdir=mytest --imin=1200 --imax=1300 --jmin=500 --jmax=600
+```
+where `$TFILE`, `$UFILE` and `$VFILE` are the names of the T, U, and V netCDF files. 
 
 ## How to compute the total flow across an irregular path
 
 ```
-cd nemoflux/nemoflux
-python fluxviz.py -t 
-
+python fluxviz.py -t ../data/sa/T.nc -u ../data/sa/U.nc -v ../data/sa/V.nc -s ../data/sa/S3_sa.txt
 ```
+![alt total flow at time 0](https://github.com/pletzer/nemoflux/blob/main/picture/sa.png?raw=true)
 
+Feel free to edit the target points in file ../data/sa/S3_sa.txt. 
---
+++ nemoflux/subsetNEMO.py
@@ -0,0 +1,88 @@
+import netCDF4
+import defopt
+
+def main(*, tfile: str='', ufile: str='', vfile: str='', outputdir: str='', jmin: int, jmax: int, imin: int, imax: int):
+    """
+    subset nemo data
+    :param tfile: name of the netCDF file containing T cell grid data
+    :param ufile: name of the netCDF file containing u data
+    :param vfile: name of the netCDF file containing v data
+    :param outputdir: output directory, the files will be saved as T.nc, U.nc and V.nc
+    :param jmin: min j index
+    :param jmax: max j index
+    :param imin: min i index
+    :param imax: max i index
+    """
+
+    # T file
+    nci = netCDF4.Dataset(tfile)
+    nco = netCDF4.Dataset(f'{outputdir}/T.nc', 'w')
+
+    for d in nci.dimensions:
+        print(f'creating dimension {d}')
+        if d == 'x':
+            nco.createDimension(d, imax - imin)
+        elif d == 'y':
+            nco.createDimension(d, jmax - jmin)
+        else:
+            nco.createDimension(d, nci.dimensions[d].size)
+
+    for vname in 'bounds_lon', 'bounds_lat', 'deptht', 'deptht_bounds':
+        print(f'creating variable {vname}')
+        vari = nci.variables[vname]
+        varo = nco.createVariable(vname, vari.dtype, vari.dimensions)
+        for a in vari.ncattrs():
+            val = getattr(vari, a)
+            print(f'\tattribute {a} has value {val}')
+            setattr(varo, a, val)
+        # write the variable
+        if 'lon' in vname or 'lat' in vname:
+            varo[:] = vari[jmin:jmax, imin:imax]
+        else:
+            varo[:] = vari[:]
+    nco.close()
+    nci.close()
+
+    # U, V file
+    varnameMap = {'U': 'uo', 'V': 'vo'}
+    filenameMap = {'U': ufile, 'V': vfile}
+
+    for field in 'U', 'V':
+        nci = netCDF4.Dataset(filenameMap[field])
+        nco = netCDF4.Dataset(f'{outputdir}/{field}.nc', 'w')
+
+        for d in nci.dimensions:
+            print(f'creating dimension {d}')
+            if d == 'x':
+                nco.createDimension(d, imax - imin)
+            elif d == 'y':
+                nco.createDimension(d, jmax - jmin)
+            else:
+                nco.createDimension(d, nci.dimensions[d].size)
+
+        fname = varnameMap[field]
+        for vname in 'time_counter', 'time_centered', 'time_centered_bounds', fname:
+            print(f'creating variable {vname}')
+            vari = nci.variables[vname]
+            if vname in ('uo', 'vo'):
+                varo = nco.createVariable(vname, vari.dtype, vari.dimensions, fill_value=vari._FillValue, zlib=True)
+            else:
+                varo = nco.createVariable(vname, vari.dtype, vari.dimensions)
+            for a in vari.ncattrs():
+                if a == '_FillValue':
+                    continue
+                val = getattr(vari, a)
+                print(f'\tattribute {a} has value {val}')
+                setattr(varo, a, val)
+            # write
+            if vname == 'uo' or vname == 'vo':
+                varo[:] = vari[..., jmin:jmax, imin:imax]
+            else:
+                varo[:] = vari[:]
+        nco.close()
+        nci.close()
+
+
+
+if __name__ == '__main__':
+    defopt.run(main)
